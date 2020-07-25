14a05e756ac13923652a30b8ae49c6c918b19076
Nikolai Wuttke <lethal_guitar128@web.de>
2020-07-25 17:46:43 +0200

    Disable warnings introduced in newer Clang versions

--- cmake/rigel.cmake
+++ cmake/rigel.cmake
@@ -55,10 +55,13 @@ function(rigel_configure_compiler_warnings)
             -Wno-switch-enum
             -Wno-unused-parameter
             -Wno-unused-lambda-capture
+            -Wno-unused-private-field
             -Wno-weak-vtables
             -Wno-global-constructors
             -Wno-float-equal
             -Wno-double-promotion
+            -Wno-ctad-maybe-unsupported
+            -Wno-implicit-int-float-conversion
         )
 
         if (WARNINGS_AS_ERRORS)
